d86e34b9241cd29bba482c8e8d61bc45438dc355
Siah Yee Long <100999280+siahyeelong@users.noreply.github.com>
2025-05-23 11:54:55 +0200

    Update data_manager.js

--- frontend/data_manager.js
+++ frontend/data_manager.js
@@ -9,29 +9,66 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     zoomOffset: -1
 }).addTo(mymap);
 
-// Add all rooms on the map
-var roomStyle = {
-    "color": "#003366",
-    "weight": 5,
-    "opacity": 0.65
-};
-var roomsFeature = []
-axios.get('http://localhost:3000/rooms')
-    .then(function (response) {
-        response.data.forEach(element => {
-            if (element.geom !== null) {
-                var geometry = JSON.parse(element.geom)
-                var feature = {
-                    "type": "Feature",
-                    "properties": {
-                        "name": "room " + element.id,
-                    },
-                    "geometry": geometry
-                };
-                roomsFeature.push(feature)
-                roomLayer = L.geoJSON(feature, { "style": roomStyle });
-                roomLayer.addTo(mymap)
 
-            }
-        });
-    })
+async function loadRooms() {
+    // Add all rooms on the map
+    var roomStyle = {
+        "color": "#003366",
+        "weight": 5,
+        "opacity": 0.65
+    };
+    var roomsFeature = []
+    axios.get('http://innovatinsa.piwio.fr:3004/rooms')
+        .then(function (response) {
+            response.data.forEach(element => {
+                if (element.geom !== null) {
+                    var geometry = JSON.parse(element.geom)
+                    var feature = {
+                        "type": "Feature",
+                        "properties": {
+                            "name": "room " + element.id,
+                        },
+                        "geometry": geometry
+                    };
+                    roomsFeature.push(feature)
+                    roomLayer = L.geoJSON(feature, { "style": roomStyle });
+                    roomLayer.addTo(mymap)
+
+                }
+            });
+        })
+        .then(console.log("rooms loaded"))
+}
+
+async function loadBuildings() {
+    // get buildings
+    var buildingStyle = {
+        "color": "#FFFF00",
+        "weight": 5,
+        "opacity": 0.65
+    };
+    var buildingsFeature = []
+    axios.get('http://innovatinsa.piwio.fr:3004/buildings')
+        .then(function (response) {
+            response.data.forEach(element => {
+                if (element.geom !== null) {
+                    var geometry = JSON.parse(element.geom)
+                    var feature = {
+                        "type": "Feature",
+                        "properties": {
+                            "name": "building " + element.id,
+                        },
+                        "geometry": geometry
+                    };
+                    buildingsFeature.push(feature)
+                    buildingLayer = L.geoJSON(feature, { "style": buildingStyle });
+                    buildingLayer.addTo(mymap)
+
+                }
+            });
+        })
+        .then(console.log("buildings loaded"))
+}
+
+loadBuildings()
+loadRooms()
